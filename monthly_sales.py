e70b53bb079eee62d6421fec6f224dc4e7871fa8
Mike Rossetti <s2t2@users.noreply.github.com>
2019-02-05 14:07:59 -0500

    Make a plan

--- monthly_sales.py
+++ monthly_sales.py
@@ -1,23 +1,42 @@
 # monthly_sales.py
 
-# TODO: import some modules and packages here
+# utility function to convert float or integer to usd-formatted string (for printing)
+# source: https://github.com/s2t2/shopping-cart-screencast/blob/30c2a2873a796b8766e9b9ae57a2764725ccc793/shopping_cart.py#L56-L59
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price) #> $12,000.71
 
-# TODO: write some Python code here to produce the desired functionality...
+#
+# INPUTS
+#
+
+sales = [] # TODO: read csv file
+
+#
+# CALCULATIONS
+#
+
+monthly_total = 12000.71 # TODO: get from sales data
+
+#
+# OUTPUTS
+#
 
 print("-----------------------")
-print("MONTH: March 2018")
+print("MONTH: March 2018") # TODO: get month and year
 
 print("-----------------------")
 print("CRUNCHING THE DATA...")
 
 print("-----------------------")
-print("TOTAL MONTHLY SALES: $12,000.71")
+print(f"TOTAL MONTHLY SALES: {to_usd(monthly_total)}")
 
 print("-----------------------")
 print("TOP SELLING PRODUCTS:")
 print("  1) Button-Down Shirt: $6,960.35")
 print("  2) Super Soft Hoodie: $1,875.00")
-print("  3) etc.")
+print("  3) etc.") # TODO: get top sellers
 
 print("-----------------------")
 print("VISUALIZING THE DATA...")
+
+# TODO: display bar chart of top sellers
