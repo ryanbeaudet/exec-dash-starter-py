f790f124895db77920e37655c91e1e5a7a424aaa
Mike Rossetti <s2t2@users.noreply.github.com>
2019-02-11 15:23:25 -0500

    Get list of unique products

--- monthly_sales_alt.py
+++ monthly_sales_alt.py
@@ -39,9 +39,37 @@ monthly_total = csv_data["sales price"].sum()
 #     {"name": "Baseball Cap", "monthly_sales": 156.31}
 # ]
 
+# breakpoint()
+# (Pdb) print(type(csv_data)) #> <class 'pandas.core.frame.DataFrame'>
+#
+# (Pdb) csv_data.head(3)
+#>          date            product  unit price  units sold  sales price
+#> 0  2018-03-01  Button-Down Shirt       65.05           2       130.10
+#> 1  2018-03-01   Vintage Logo Tee       15.95           1        15.95
+#> 2  2018-03-01       Sticker Pack        4.50           1         4.50
+#
+# Get unique products
+#
+product_names = csv_data["product"]
+# ... what kind of datatype is this?
+# (Pdb) print(type(product_names)) #> <class 'pandas.core.series.Series'>
+# ... google search for "pandas.core.series.Series unique values" yields: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.unique.html
+unique_product_names = product_names.unique() # :-D
+# (Pdb) print(unique_product_names) #> ['Button-Down Shirt' 'Vintage Logo Tee' 'Sticker Pack' 'Super Soft Hoodie' 'Baseball Cap' 'Khaki Pants' 'Brown Boots']
+# ... looks like a list, but what kind of datatype is this?
+# (Pdb) print(type(unique_product_names)) #> <class 'numpy.ndarray'>
+# ... numpy.ndarray WAT? let's try to convert it to a list, if possible, so we can be working with familiar datatypes...
+# ... google search for "how to convert numpy.ndarray to list" yields:
+#  + https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.ndarray.tolist.html
+#  + https://stackoverflow.com/questions/1966207/converting-numpy-array-into-python-list-structure
+unique_product_names = unique_product_names.tolist() # convert numpy.ndarray to list
+
+# Accumulate total sales per product
+
 top_sellers = []
 
 breakpoint()
+
 
 #
 # OUTPUTS
