198861318adbaead6d32ca1cdb0ba34162bbcf3f
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-04 13:30:07 -0400

    Satisfy expectations for top products (green light)

--- app/monthly_sales_alt.py
+++ app/monthly_sales_alt.py
@@ -11,60 +11,36 @@ import matplotlib.ticker as ticker
 def to_usd(my_price):
     return "${0:,.2f}".format(my_price) #> $12,000.71
 
-def top_selling_products():
-    return [1,2,3]
+# expects "sales_data" to be a pandas dataframe with headers: "product", "sales price", etc.
+def top_selling_products(sales_data):
+    product_names = sales_data["product"]
+    unique_product_names = product_names.unique()
+    unique_product_names = unique_product_names.tolist() # converts numpy.ndarray to list
+    top_sellers = []
+    for product_name in unique_product_names:
+        matching_rows = sales_data[sales_data["product"] == product_name]
+        product_monthly_sales = matching_rows["sales price"].sum()
+        top_sellers.append({"name": product_name, "monthly_sales": product_monthly_sales})
+    top_sellers = sorted(top_sellers, key=operator.itemgetter("monthly_sales"), reverse=True)
+    return top_sellers
 
 if __name__ == "__main__":
 
-    #
     # INPUTS
-    #
 
     csv_filename = "sales-201803.csv" # TODO: allow user to specify
 
-    # reference a file in the "data" directory, adapted from:
-    #  + https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/modules/os.md#file-operations
     csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", csv_filename)
 
-    # read csv file into a pandas dataframe object, adapted from:
-    #  + https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/packages/pandas.md
     csv_data = pandas.read_csv(csv_filepath)
 
-    #
     # CALCULATIONS
-    #
 
     monthly_total = csv_data["sales price"].sum()
 
-    # get unique product names, adapted from:
-    #  + https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.ndarray.tolist.html
-    #  + https://stackoverflow.com/questions/1966207/converting-numpy-array-into-python-list-structure
-    product_names = csv_data["product"]
-    unique_product_names = product_names.unique()
-    unique_product_names = unique_product_names.tolist() # convert numpy.ndarray to list
+    top_sellers = top_selling_products(csv_data)
 
-    top_sellers = []
-
-    for product_name in unique_product_names:
-        # filering approach adapted from: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/packages/pandas.md
-        matching_rows = csv_data[csv_data["product"] == product_name]
-        product_monthly_sales = matching_rows["sales price"].sum()
-        top_sellers.append({"name": product_name, "monthly_sales": product_monthly_sales})
-
-    # sort the list of dictionaries to make sure they are looped through and charted in the right order, adapted from:
-    #   + https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/datatypes/lists.md#sorting
-    # sort in descending order, adapted from:
-    #  + https://www.programiz.com/python-programming/methods/built-in/sorted
-    #  + https://docs.python.org/3/howto/sorting.html
-    top_sellers = sorted(top_sellers, key=operator.itemgetter("monthly_sales"), reverse=True)
-
-    breakpoint() # what do we expect top_sellers to look like?
-    #> [{'name': 'Button-Down Shirt', 'monthly_sales': 6960.35}, {'name': 'Super Soft Hoodie', 'monthly_sales': 1875.0}, {'name': 'Khaki Pants', 'monthly_sales': 1602.0}, {'name': 'Vintage Logo Tee', 'monthly_sales': 941.0500000000001}, {'name': 'Brown Boots', 'monthly_sales': 250.0}, {'name': 'Sticker Pack', 'monthly_sales': 216.0}, {'name': 'Baseball Cap', 'monthly_sales': 156.31}]
-
-
-    #
     # OUTPUTS
-    #
 
     print("-----------------------")
     print("MONTH: March 2018") # TODO: get month and year
@@ -85,20 +61,6 @@ if __name__ == "__main__":
 
     print("-----------------------")
     print("VISUALIZING THE DATA...")
-
-    # dataviz adapted from:
-    #  + https://georgetown-opim-py.slack.com/archives/CFZDKNKA4/p1549494877005200
-    #  + https://matplotlib.org/api/_as_gen/matplotlib.pyplot.title.html
-    #  + https://matplotlib.org/gallery/subplots_axes_and_figures/figure_title.html
-    #  + https://matplotlib.org/users/pyplot_tutorial.html
-    #  + https://matplotlib.org/gallery/lines_bars_and_markers/barh.html
-    #  + https://python-graph-gallery.com/2-horizontal-barplot/
-    #  + https://matplotlib.org/api/_as_gen/matplotlib.pyplot.margins.html
-    #  + https://stackoverflow.com/questions/6774086/why-is-my-xlabel-cut-off-in-my-matplotlib-plot
-    #  + https://matplotlib.org/gallery/pyplots/dollar_ticks.html
-    #  + https://stackoverflow.com/questions/38152356/matplotlib-dollar-sign-with-thousands-comma-tick-labels
-    #  + https://pbpython.com/effective-matplotlib.html#customizing-the-plot
-    #  + https://matplotlib.org/api/_as_gen/matplotlib.pyplot.subplots.html#matplotlib.pyplot.subplots
 
     chart_title = "Top Selling Products (March 2018)" #TODO: get month and year
 
--- test/monthly_sales_alt_test.py
+++ test/monthly_sales_alt_test.py
@@ -1,5 +1,8 @@
 
 
+import os
+import pandas
+
 from app.monthly_sales_alt import to_usd, top_selling_products
 
 def test_to_usd():
@@ -17,6 +20,10 @@ def test_to_usd():
 
 def test_top_selling_products():
     # it should return a list of names and sales totals, sorted by sales ascending with top sellers first:
+    csv_filename = "sales-201803.csv"
+    csv_filepath = os.path.join(os.path.dirname(__file__), "mock_data", csv_filename)
+    csv_data = pandas.read_csv(csv_filepath)
+    results = top_selling_products(csv_data)
     expected_result = [
         {'name': 'Button-Down Shirt', 'monthly_sales': 6960.35},
         {'name': 'Super Soft Hoodie', 'monthly_sales': 1875.0},
@@ -26,4 +33,4 @@ def test_top_selling_products():
         {'name': 'Sticker Pack', 'monthly_sales': 216.0},
         {'name': 'Baseball Cap', 'monthly_sales': 156.31}
     ]
-    assert top_selling_products() == expected_result
+    assert results == expected_result
