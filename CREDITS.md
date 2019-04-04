39843aa951e8c353de43aaa7474ee47017ce2c62
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-04 13:51:47 -0400

    Prepare to test original solution

--- CREDITS.md
+++ CREDITS.md
@@ -11,6 +11,10 @@
 
   + https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.ndarray.tolist.html
   + https://stackoverflow.com/questions/1966207/converting-numpy-array-into-python-list-structure
+  + https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas
+  + http://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.iterrows.html
+  + http://pandas.pydata.org/pandas-docs/version/0.19/generated/pandas.DataFrame.sort.html
+  + https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.groupby.html
 
 ## Matplotlib
 
@@ -26,6 +30,21 @@
   + https://stackoverflow.com/questions/38152356/matplotlib-dollar-sign-with-thousands-comma-tick-labels
   + https://pbpython.com/effective-matplotlib.html#customizing-the-plot
   + https://matplotlib.org/api/_as_gen/matplotlib.pyplot.subplots.html#matplotlib.pyplot.subplots
+
+## Plotly
+
+  + https://plot.ly/python/bar-charts/
+  + https://plot.ly/python/getting-started
+  + https://plot.ly/python/getting-started/#initialization-for-offline-plotting
+  + https://plot.ly/python/horizontal-bar-charts/
+  + https://plot.ly/python/axes/#reversed-axes
+  + https://community.plot.ly/t/adjusting-width-of-y-axis-labels-in-heatmap/3684
+  + https://plot.ly/python/reference/#layout-margin
+  + https://plot.ly/python/setting-graph-size/
+  + https://plot.ly/python/setting-graph-size/#automatically-adjust-margins
+  + https://plot.ly/python/bar-charts/#bar-chart-with-direct-labels
+  + https://community.plot.ly/t/bar-chart-with-both-direct-labels-and-hover-text/7528
+
 
 ## Pytest
 
--- app/monthly_sales.py
+++ app/monthly_sales.py
@@ -10,115 +10,86 @@ from plotly import graph_objs
 def to_usd(my_price):
     return "${0:,.2f}".format(my_price) #> $12,000.71
 
-#
-# INPUTS
-#
+if __name__ == "__main__":
 
-csv_filename = "sales-201803.csv" # TODO: allow user to specify
+    # INPUTS
 
-# reference a file in the "data" directory
-# ... adapted from: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/modules/os.md#file-operations
-csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", csv_filename)
+    csv_filename = "sales-201803.csv" # TODO: allow user to specify
 
-# read csv file into a pandas dataframe object
-# ... this and other pandas operations adapted from: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/packages/pandas.md
-csv_data = pandas.read_csv(csv_filepath)
+    csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", csv_filename)
 
-#
-# CALCULATIONS
-#
+    csv_data = pandas.read_csv(csv_filepath)
 
-monthly_total = csv_data["sales price"].sum()
+    # CALCULATIONS
 
-# google search for "pandas group by" leads to...
-# ... https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.groupby.html
-product_totals = csv_data.groupby(["product"]).sum()
+    monthly_total = csv_data["sales price"].sum()
 
-# google search for "pandas dataframe order rows" leads to ...
-# ... http://pandas.pydata.org/pandas-docs/version/0.19/generated/pandas.DataFrame.sort.html
-product_totals = product_totals.sort_values("sales price", ascending=False)
+    product_totals = csv_data.groupby(["product"]).sum()
 
-# google search for "pandas loop through each row in a dataframe" results in ...
-# ... https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas
-# ... http://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.iterrows.html
+    product_totals = product_totals.sort_values("sales price", ascending=False)
 
-top_sellers = []
-rank = 1
-for i, row in product_totals.iterrows():
-    d = {"rank": rank, "name": row.name, "monthly_sales": row["sales price"]}
-    top_sellers.append(d)
-    rank = rank + 1
+    top_sellers = []
+    rank = 1
+    for i, row in product_totals.iterrows():
+        d = {"rank": rank, "name": row.name, "monthly_sales": row["sales price"]}
+        top_sellers.append(d)
+        rank = rank + 1
 
-#
-# OUTPUTS
-#
+    # OUTPUTS
 
-print("-----------------------")
-print("MONTH: March 2018") # TODO: get month and year
+    print("-----------------------")
+    print("MONTH: March 2018") # TODO: get month and year
 
-print("-----------------------")
-print("CRUNCHING THE DATA...")
+    print("-----------------------")
+    print("CRUNCHING THE DATA...")
 
-print("-----------------------")
-print(f"TOTAL MONTHLY SALES: {to_usd(monthly_total)}")
+    print("-----------------------")
+    print(f"TOTAL MONTHLY SALES: {to_usd(monthly_total)}")
 
-print("-----------------------")
-print("TOP SELLING PRODUCTS:")
-for d in top_sellers:
-    print("  " + str(d["rank"]) + ") " + d["name"] + ": " + to_usd(d["monthly_sales"]))
+    print("-----------------------")
+    print("TOP SELLING PRODUCTS:")
+    for d in top_sellers:
+        print("  " + str(d["rank"]) + ") " + d["name"] + ": " + to_usd(d["monthly_sales"]))
 
-print("-----------------------")
-print("VISUALIZING THE DATA...")
+    print("-----------------------")
+    print("VISUALIZING THE DATA...")
 
-# adapted from:
-# ... https://plot.ly/python/bar-charts/
-# ... https://plot.ly/python/getting-started
-# ... https://plot.ly/python/getting-started/#initialization-for-offline-plotting
-# ... https://plot.ly/python/horizontal-bar-charts/
-# ... https://plot.ly/python/axes/#reversed-axes
-# ... https://community.plot.ly/t/adjusting-width-of-y-axis-labels-in-heatmap/3684
-# ... https://plot.ly/python/reference/#layout-margin
-# ... https://plot.ly/python/setting-graph-size/
-# ... https://plot.ly/python/setting-graph-size/#automatically-adjust-margins
-# ... https://plot.ly/python/bar-charts/#bar-chart-with-direct-labels
-# ... https://community.plot.ly/t/bar-chart-with-both-direct-labels-and-hover-text/7528
+    sorted_product_names = [d["name"] for d in top_sellers] # list comprehension for mapping purposes!
+    sorted_product_sales = [d["monthly_sales"] for d in top_sellers] # list comprehension for mapping purposes!
+    sorted_bar_labels = [to_usd(d["monthly_sales"]) for d in top_sellers] # list comprehension for mapping purposes!
 
-sorted_product_names = [d["name"] for d in top_sellers] # list comprehension for mapping purposes!
-sorted_product_sales = [d["monthly_sales"] for d in top_sellers] # list comprehension for mapping purposes!
-sorted_bar_labels = [to_usd(d["monthly_sales"]) for d in top_sellers] # list comprehension for mapping purposes!
+    data = [
+        graph_objs.Bar(
+            x=sorted_product_sales,
+            y=sorted_product_names,
+            orientation="h", # orients bars horizontally
+            text=sorted_bar_labels, # display bar label (USD-formatted string version of the product sales)
+            textposition= "auto", # I prefer "outside", but it causes the label on the biggest bar to get cut off
+            hoverinfo="text" # prevents duplication of sales price in hover text
+        )
+    ]
 
-data = [
-    graph_objs.Bar(
-        x=sorted_product_sales,
-        y=sorted_product_names,
-        orientation="h", # orients bars horizontally
-        text=sorted_bar_labels, # display bar label (USD-formatted string version of the product sales)
-        textposition= "auto", # I prefer "outside", but it causes the label on the biggest bar to get cut off
-        hoverinfo="text" # prevents duplication of sales price in hover text
+    chart_title = "Top Selling Products (March 2018)" # TODO: get month and year
+
+    layout = graph_objs.Layout(
+        title=chart_title,
+        xaxis=dict(tickformat="$"), # thought it might be possible to format USD here... # "${:,.0f}" # "${0:,.2f}" # ",.2f" # "$$$$$$$" # "${0:,.2f}" # ".2%"
+        yaxis=dict(autorange="reversed"), # ensures top sellers are on the top
+        margin=graph_objs.layout.Margin(l=150, pad=20) # ensures we can see the full product names
     )
-]
 
-chart_title = "Top Selling Products (March 2018)" # TODO: get month and year
+    chart_options = {"data": data, "layout": layout}
 
-layout = graph_objs.Layout(
-    title=chart_title,
-    xaxis=dict(tickformat="$"), # thought it might be possible to format USD here... # "${:,.0f}" # "${0:,.2f}" # ",.2f" # "$$$$$$$" # "${0:,.2f}" # ".2%"
-    yaxis=dict(autorange="reversed"), # ensures top sellers are on the top
-    margin=graph_objs.layout.Margin(l=150, pad=20) # ensures we can see the full product names
-)
+    chart_filename = "top-sellers-201803.html" # TODO: parse selected csv file name
+    chart_filepath = os.path.join(os.path.dirname(__file__), "..", "reports", chart_filename)
 
-chart_options = {"data": data, "layout": layout}
+    plotly.offline.plot(
+        chart_options,
+        filename=chart_filepath,
+        #image="png",
+        #image_filename="top-sellers-201803", # prefer to download image file into this repo, but download location seems to not be customizable
+        auto_open=True
+    )
 
-chart_filename = "top-sellers-201803.html" # TODO: parse selected csv file name
-chart_filepath = os.path.join(os.path.dirname(__file__), "..", "reports", chart_filename)
-
-plotly.offline.plot(
-    chart_options,
-    filename=chart_filepath,
-    #image="png",
-    #image_filename="top-sellers-201803", # prefer to download image file into this repo, but download location seems to not be customizable
-    auto_open=True
-)
-
-print("-----------------------")
-print("SAVED REPORT TO: " + chart_filepath)
+    print("-----------------------")
+    print("SAVED REPORT TO: " + chart_filepath)
